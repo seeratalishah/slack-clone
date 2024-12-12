eea6ae3a514dbfa8680277a8174cf6d114f28a4f
Seerat Ali <58472058+seeratalishah@users.noreply.github.com>
2024-12-12 12:05:42 +0500

    Completed UI for auth modules

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import AuthScreen from "@/features/auth/components/auth-screen";
 
 export default function Home() {
   return (
-   <div className="text-rose-500">
-   <AuthScreen />
-   </div>
+    <div>
+      <AuthScreen />
+    </div>
   );
 }
